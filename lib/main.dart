73103c09458694ab61a721af85c510eccb45a702
c.skeim <cskeim@css.edu>
2021-03-24 16:29:28 -0500

    Completed Activity

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  //Build Method
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,13 +35,17 @@ class MyHomePage extends StatefulWidget {
 //Our code goes in this method
 class _MyHomePageState extends State<MyHomePage> {
 
+  //Declare and Initialize textField controllers, creates and connects the widgets
   final dollarTextField = TextEditingController();
   final euroTextField = TextEditingController();
 
+  //Variables to be used in conversions
   double _dollars = 0;
   double _euros = 0;
   double _conversionRate = 1.19;
 
+  //Method to convert euros to dollars
+  //Call the set state method because this method is changing the display on the app
   void _convertToDollars(){
     setState(() {
     _euros = double.parse(euroTextField.text);
@@ -65,12 +70,14 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Text(
               'Dollars:',
+              //Style attribute to increase the widget size
               style: Theme.of(context).textTheme.headline4,
             ),
             TextField(
               controller: dollarTextField,
             ),
             ElevatedButton(
+                //Convert dollars to euros when the button is clicked
                 onPressed: (){
                   _dollars = double.parse(dollarTextField.text);
                   _euros = _dollars * _conversionRate;
